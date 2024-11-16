f280134ba3f6a5aeedc688a1eb4c93d7c8579a61
cheechyuanang <angchee@live.com.my>
2024-11-16 21:38:45 +0700

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      evmVersion: "cancun",
+    },
+  },
+};
+
+export default config;
